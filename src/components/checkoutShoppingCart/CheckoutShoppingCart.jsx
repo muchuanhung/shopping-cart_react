a281a426285e7afdcccfa0ae3248ae8c6f8051b8
muchuan <mu.chuan.hung@gmail.com>
2022-07-03 16:40:07 +0800

    structure setup

---
+++ src/components/checkoutShoppingCart/CheckoutShoppingCart.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Footer() {
+  return (
+    <div className="">
+      <li className="">我是shoppingcart</li>;
+    </div>
+  );
+}
---
+++ src/components/stepper/Stepper.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Footer() {
+  return (
+    <div className="">
+      <li className="">我是stepper</li>;
+    </div>
+  );
+}
